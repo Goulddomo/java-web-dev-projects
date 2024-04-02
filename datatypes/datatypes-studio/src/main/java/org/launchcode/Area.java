30749b73abb4facd1368b1ae47c05e68deb10e3e
Dominique Gould <dominiquegould42@yahoo.com>
2024-04-01 20:26:53 -0500

    Studio Done

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -2,10 +2,12 @@ package org.launchcode;
 
 import java.util.Scanner;
 
+import static java.lang.Float.isNaN;
+
 public class Area {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the radius of the circle: ");
         double radius = input.nextDouble();
@@ -13,6 +15,14 @@ public class Area {
         double area = Circle.getArea(radius);
 //        double area = (radius * radius) * Math.PI;
         System.out.println("The area of the circle is: " + area + ".");
-    }
+//in
+//
+//        if (radius.isNaN) {
+//
+//            // the "then" clause: ask to submit a correct int
+//        }
+//    }
 
+
+    }
 }
